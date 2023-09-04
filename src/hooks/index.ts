fd33cad57cfeb7c352709984362248b61237110c
qyj <1078869122@qq.com>
2023-09-04 15:43:22 +0800

    fix: remove usevisibility

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,6 @@ import { useClassName } from './useClassName'
 import { useWindowEventListener } from './useWindowEventListener'
 import { useOppsite } from './useOppsite'
 import { useDropdownPosition } from './useDropdownPosition'
-import { useVisibility } from './useVisibility'
 import { useEventListener } from './useEventListener'
 
-export { useEventListener, useClassName, useWindowEventListener, useOppsite, useDropdownPosition, useVisibility }
+export { useEventListener, useClassName, useWindowEventListener, useOppsite, useDropdownPosition }
