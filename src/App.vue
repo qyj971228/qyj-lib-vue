71adeee212a22496a49d602aa91f2eed6af25c07
qyj <1078869122@qq.com>
2023-09-05 16:19:25 +0800

    stage: menu-02

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,18 @@ import Menu from './components/menu/index.vue'
 
 <template>
   <!-- <DropdownDemoPosition></DropdownDemoPosition> -->
-  <Menu></Menu>
+  <Menu
+    :list="[
+      {
+        name: '1',
+        children: [{ name: '1-1' }, { name: '1-2' }, { name: '1-3' }, { name: '1-4' }]
+      },
+      {
+        name: '2',
+        children: [{ name: '2-1' }]
+      }
+    ]"
+  ></Menu>
 </template>
 
 <style>
--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, toRef } from 'vue'
+
+const props = defineProps<MenuProps>()
+
+type MenuProps = {
+  list: List[]
+}
 
 interface List {
   name: string
@@ -9,79 +15,69 @@ interface List {
   isCollapsed?: boolean
 }
 
-const list = ref<List[]>([
-  {
-    name: '1',
-    children: [
-      {
-        name: '1-1'
-      },
-      {
-        name: '1-2'
-      }
-    ]
-  },
-  {
-    name: '2',
-    children: [{ name: '2-1' }]
-  }
-])
-const data = computed(() => {
-  const arr: List[] = []
-  for (let i = 0; i < list.value.length; i++) {
-    const item: List = list.value[i]
-    if (item.children && item.children.length > 0) {
-      item.children.forEach((el: List) => {
-        el.className = 'qyj-menu-level-secondary'
-        el.collapsible = false
-        el.isCollapsed = false
-      })
-      arr[i] = item
-      arr[i].children?.unshift({ name: arr[i].name, className: 'qyj-menu-level-main', collapsible: true })
+const list = toRef(props.list)
+
+const listData = computed(() => {
+  let arr: List[] = []
+  arr = list.value.map((item: List) => {
+    if (!item.children || item.children.length == 0) {
+      item = { ...item, children: [] }
     }
-  }
+    item.children?.forEach((el: List) => { // 二级菜单状态与样式
+      el.className = 'qyj-menu-level-secondary'
+      el.collapsible = false
+      el.isCollapsed = false
+    })
+    item.children?.unshift({ name: item.name, className: 'qyj-menu-level-main', collapsible: true }) // 一级菜单状态与样式
+    return item
+  })
   return arr
 })
 
 const menuGroup = ref<HTMLElement[] | null>()
 const menuItem = ref<HTMLElement[] | null>()
 
-function onclickMenuItem(item: List, index: number) {
+function onClickPrimaryItem(item: List, index: number) {
+  console.log(item, index)
+  if (item.children?.length == 1) return
   if (!(menuGroup.value && menuItem.value)) return
-  else {
-    if (!item.collapsible && !list.value[index].isCollapsed) {
-      // 如果有item折叠到item的高度
-      // 没有则不折叠
-      const height = menuItem.value[0].clientHeight
-      menuGroup.value[index].style.height = height + 'px'
-      list.value[index].isCollapsed = true
-      return
-    }
-    if (!item.collapsible && list.value[index].isCollapsed) {
-      const height = menuItem.value[0].clientHeight
-      menuGroup.value[index].style.height = height * (list.value[index].children?.length ?? 0) + 'px'
-      list.value[index].isCollapsed = false
-      return
-    }
+  // 未折叠
+  if (!item.collapsible && !list.value[index].isCollapsed) {
+    const height = menuItem.value[0].clientHeight // 一级菜单的高度
+    menuGroup.value[index].style.height = height + 'px' // 菜单分组高度只显示一级菜单
+    list.value[index].isCollapsed = true // 变为折叠状态
+    return
   }
+  // 已折叠
+  if (!item.collapsible && list.value[index].isCollapsed) {
+    const primaryHeight = menuItem.value[0].clientHeight // 一级菜单的高度
+    const secondaryHeight = menuItem.value[1].clientHeight // 二级菜单的高度
+    menuGroup.value[index].style.height = primaryHeight + secondaryHeight * ((list.value[index].children?.length ?? 1) - 1) + 'px'
+    list.value[index].isCollapsed = false // 变为折叠状态
+    return
+  }
+}
+
+function onClickSecondaryItem(item: List, index: number) {
+  console.log(item, index)
 }
 </script>
 <template>
   <div class="qyj-menu">
     <ul
       class="qyj-menu-group"
-      v-for="(item, index) in data"
-      :key="index"
+      v-for="(primaryItem, primaryIndex) in listData"
+      :key="primaryIndex"
       ref="menuGroup"
     >
       <li
-        :class="child.className"
-        v-for="(child, i) in item.children"
-        :key="i"
-        @click="onclickMenuItem(item, index)"
+        :class="secondaryItem.className"
+        v-for="(secondaryItem, secondaryIndex) in primaryItem?.children"
+        :key="secondaryIndex"
+        @click="secondaryIndex == 0 ? onClickPrimaryItem(primaryItem, primaryIndex) : onClickSecondaryItem(secondaryItem, secondaryIndex)"
         ref="menuItem"
       >
-        {{ child.name }}
+        {{ secondaryItem.name }}
       </li>
     </ul>
   </div>
