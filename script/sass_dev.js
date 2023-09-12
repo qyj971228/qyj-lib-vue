0d508f5a724d9a8d665458d3f8774756072d08f5
qyj971228 <1078869122@qq.com>
2023-09-12 22:01:00 +0800

    build: sass build

---
+++ script/sass_dev.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const fs = require('fs')
+const sass = require('sass')
+const path = require('path')
+const ENTRY_PATH = path.join(__dirname, '../src') // 入口
+const SASS_FILE_NAME = 'index.scss'
+
+function sassCompile(path) {
+  return sass.compile(path, { style: 'expanded' }).css
+}
+
+function pathTransform(path) {
+  switch (process.platform) {
+    case 'darwin':
+      return path.substring(0, path.length - 4) + 'css' // mac
+    case 'win32':
+      return path.substring(0, path.length - 3) + 'css' // windows (待路径测试)
+  }
+}
+
+function fileWrite(path) {
+  fs.writeFileSync(pathTransform(path), sassCompile(path), 'utf-8', function (error) {
+    console.log(error)
+  })
+}
+
+function readFileList(currentPath) {
+  const files = fs.readdirSync(currentPath)
+  files.forEach((file) => {
+    const childPath = path.join(currentPath, file)
+    const stat = fs.statSync(childPath)
+    if (stat.isDirectory()) readFileList(childPath)
+    else if (file === SASS_FILE_NAME) fileWrite(childPath)
+  })
+}
+
+readFileList(ENTRY_PATH)
