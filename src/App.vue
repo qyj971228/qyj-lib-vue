3f4dea377999f6ded2813237948f8ef337a76f15
qyj971228 <1078869122@qq.com>
2024-01-17 18:41:53 +0800

    feat: demo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-// import DropdownDemoPosition from './components/dropdown/demo/position.vue'
+import DropdownDemoPosition from './components/dropdown/demo/position.vue'
 import MenuDemoData from './components/menu/demo/MenuDemoData.vue'
 </script>
 
 <template>
-  <!-- <DropdownDemoPosition></DropdownDemoPosition> -->
+  <DropdownDemoPosition></DropdownDemoPosition>
   <hr />
   <MenuDemoData></MenuDemoData>
 </template>
