3858995e0a6c257f79c759f53db6886d166deff8
qyj <1078869122@qq.com>
2024-02-11 00:40:31 +0800

    build: 0.1.26

--- dist/components/dropdown-item/index.vue.js
+++ dist/components/dropdown-item/index.vue.js
@@ -1,13 +1,13 @@
-import { openBlock as o, createElementBlock as t, renderSlot as r } from "vue";
+import { openBlock as e, createElementBlock as t, renderSlot as r } from "vue";
 import "./index.vue2.js";
 import n from "../../_virtual/_plugin-vue_export-helper.js";
-const c = {}, d = { class: "qyj-dropdown-item" };
-function s(e, f) {
-  return o(), t("div", d, [
-    r(e.$slots, "default", {}, void 0, !0)
+const c = {}, s = { class: "qyj-dropdown-item" };
+function d(o, m) {
+  return e(), t("div", s, [
+    r(o.$slots, "default")
   ]);
 }
-const m = /* @__PURE__ */ n(c, [["render", s], ["__scopeId", "data-v-0fa1f082"]]);
+const l = /* @__PURE__ */ n(c, [["render", d]]);
 export {
-  m as default
+  l as default
 };
