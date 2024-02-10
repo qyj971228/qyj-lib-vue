eddd64fe321dac1ee619d0f9bd229b66fdadf4e5
qyj <1078869122@qq.com>
2024-02-10 23:55:48 +0800

    feat: update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
 import Button from '../components/button/index.vue'
 import Dropdown from '../components/dropdown/index.vue'
 import DropdownItem from '../components/dropdown-item/index.vue'
+import Menu from '../components/menu/index.vue'
+import MenuSub from '../components/menu-sub/index.vue'
 
-export { Button, Dropdown, DropdownItem }
+export { Button, Dropdown, DropdownItem, Menu, MenuSub }
